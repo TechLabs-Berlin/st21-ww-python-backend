b783887baaaf1531b453d5eb58b80f0886d56870
Daniel da Rocha <danrocha@gmail.com>
2021-05-16 08:10:21 +0200

    add download script for data

--- app.py
+++ app.py
@@ -41,17 +41,6 @@ class TaskAPI(Resource):
 api.add_resource(TaskListAPI, '/todo/api/v2/tasks')
 api.add_resource(TaskAPI, '/todo/api/v2/tasks/<int:id>')
 
-@app.route('/todo/api/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error':'Not found'}), 404)
