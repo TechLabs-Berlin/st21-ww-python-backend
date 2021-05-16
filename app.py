8e5db9ccff6b285a4c49672ffad14203ccee4e27
Daniel da Rocha <danrocha@gmail.com>
2021-05-16 08:04:55 +0200

    use flask_restful for routes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, abort, make_response
-
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+from flask_restful import Api, Resource, fields
 app = Flask(__name__)
+
+api = Api(app)
 
 tasks = [
     {
@@ -8,7 +12,8 @@ tasks = [
         'title': 'Buy groceries',
         'description': 'Milk, cheese, bananas',
         'done': False
-    },{
+    },
+    {
         'id': 2,
         'title': 'Learn Python',
         'description': 'Need to learn python to finish the project',
@@ -16,9 +21,25 @@ tasks = [
     }
 ]
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+class TaskListAPI(Resource):
+    def get(self):
+        return {'tasks': tasks}
+    def put(self):
+        pass
+
+class TaskAPI(Resource):
+    def get(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        return {'task': task[0]}
+    def put(self, id):
+        pass
+    def delete(self, id):
+        pass
+
+api.add_resource(TaskListAPI, '/todo/api/v2/tasks')
+api.add_resource(TaskAPI, '/todo/api/v2/tasks/<int:id>')
 
 @app.route('/todo/api/tasks', methods=['GET'])
 def get_tasks():
@@ -34,6 +55,10 @@ def get_task(task_id):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error':'Not found'}), 404)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
