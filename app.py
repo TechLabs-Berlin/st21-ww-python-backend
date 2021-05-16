64907ff9834c762a471f0aacd3396925e272c5bd
Daniel da Rocha <danrocha@gmail.com>
2021-05-16 07:32:16 +0200

    add endpoint with data processing

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/raw-data/describe')
+def raw_data():
+    nba = pd.read_csv("data/nba_all_elo.csv")
+    info = nba.describe()
+    return info.to_json()
+
 if __name__ == '__main__':
     app.run(debug=True)
