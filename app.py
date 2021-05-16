3c6218b572492763944172fe964b7864eef3c24c
Daniel da Rocha <danrocha@gmail.com>
2021-05-16 07:43:05 +0200

    add single task endpoint

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify
-import pandas as pd
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -25,7 +24,12 @@ def index():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-
+@app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
